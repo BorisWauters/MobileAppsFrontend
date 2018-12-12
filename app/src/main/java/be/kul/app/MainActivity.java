90df6fdf52b19d90724eea8da3db7b08dbf2527c
Boris Wauters <boris.wauters@telenet.be>
2018-12-12 16:59:21 +0100

    BroadcastReceiver now with mobile internet

--- app/src/main/java/be/kul/app/MainActivity.java
+++ app/src/main/java/be/kul/app/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -88,7 +87,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private NetworkInfo getNetworkInfo(Context context) {
         ConnectivityManager connManager = (ConnectivityManager)
                 context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        return connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        NetworkInfo mobile = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifi.isConnected()) return wifi;
+        else return mobile;
+
+        //return wifi;
+
     }
 
     @Override
